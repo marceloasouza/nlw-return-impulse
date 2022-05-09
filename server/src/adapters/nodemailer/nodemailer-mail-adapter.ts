513860e07dde2d5b15351f6ec823c7186be6b585
Marcelo Souza <marceloamorsouza@yahoo.com.br>
2022-05-09 00:15:10 -0300

    NLW impulse

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "d0cf3def66c249",
+    pass: "fcf3d31e41b980"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Mcl Souza <mclsouza@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
